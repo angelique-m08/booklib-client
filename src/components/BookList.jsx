f36a0c3a384a77e59f36779c7fcc00b202d884f0
angelique-m08 <mora.angelique@gmail.com>
2020-07-22 19:46:14 +0200

    feat(booklist): create booklist file

---
+++ src/components/BookList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BookList = () => {
+  return(
+    <div>
+      <ul>
+        <li>Livre 1</li>
+        <li>Livre 2</li>
+      </ul>
+    </div>
+  )
+}
+
+export default BookList;
