4373f0cdd7de0c91ca02bb502a3702d39981e7ac
angelique-m08 <mora.angelique@gmail.com>
2020-07-23 19:22:44 +0200

    feat(navbar): add burger menu and responsive css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Link
 } from "react-router-dom";
+import Burger from 'react-css-burger';
 import './navbar.css';
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+
   return (
-    <nav className="navbar">
-    <ul className="navbarMenu">
-      <li className="navbarTitleApp"><Link to="/">BookLib</Link></li>
-      <li><Link to="/books">Livres</Link></li>
-      <li><Link to="/authors">Auteurs</Link></li>
-      <li><Link to="/createbooks">Ajouter un livre</Link></li>
-      <li><Link to="/createauthors">Ajouter un auteur</Link></li>
-    </ul>
-  </nav>
+    <>
+      <nav className="navbar">
+        <h1 className="navbarTitleApp"><Link to="/">BookLib</Link></h1>
+        <Burger
+          onClick={() => setActive(!active)}
+          active={active}
+          burger="slider"
+          color="white"
+          className="navbarBurger"
+        />
+        {
+        active === true ?
+          <div>
+            <ul className="navbarMenu">
+              <li><Link to="/books">Livres</Link></li>
+              <li><Link to="/authors">Auteurs</Link></li>
+              <li><Link to="/createbooks">Ajouter un livre</Link></li>
+              <li><Link to="/createauthors">Ajouter un auteur</Link></li>
+            </ul>
+          </div>
+          : ''
+        }
+    </nav>
+  </>
   )
 }
 
