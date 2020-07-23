35312387280337476b234f54a6841053f9d21c4a
angelique-m08 <mora.angelique@gmail.com>
2020-07-23 09:27:08 +0200

    feat(bookcreate): add form to add book

--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './bookCreate.css'
 
 const BookCreate = () => {
   return (
     <div>
-      Hello
+      <h1 className="bookCreateTitle">Ajouter un nouveau livre</h1>
+      <form className="bookCreateForm">
+        <label for="title">Titre</label>
+        <input id="title" name="title" required />
+        <label for="author">Auteur</label>
+        <input id="author" name="author" />
+        <label for="resume">Résumé</label>
+        <textarea id="resume" name="resume"></textarea>
+        <label for="cover">Lien vers une image de couverture</label>
+        <input id="cover" name="cover" />
+        <button type="button" className="bookCreateFormButton">Ajouter</button>
+      </form>
     </div>
   )
 }
