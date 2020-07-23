d53756aa1864046a40aaf02b00a35c90224aa6df
angelique-m08 <mora.angelique@gmail.com>
2020-07-23 10:41:53 +0200

    feat(book): add axios call to delete one book

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
-
+import Axios from 'axios';
 const Book = ({ book }) => {
+
+  const deleteBook = () => {
+    Axios.delete(`http://localhost:8000/books/${book.id}`)
+    .then(res => alert("Livre supprimé"))
+  }
+
   return(
-    <div>
-      <img src={book.cover} alt={book.title} />
-      <p className="bookTitle">{book.title}</p>
-      <p className="bookResume">Résumé :<br></br>{book.resume}</p>
-      <p>Les avis sur le livre :</p>
-      <ul className="bookReview">
-        <li>Avis 1</li>
-        <li>Avis 2</li>
-      </ul>
-      <form>
-        <label for="review">Laissez votre avis</label>
-        <textarea id="review" name="review"></textarea>
-        <button type="submit">Ajouter</button>
-      </form>
+    <div className="bookDiv">
+      <div className="bookLeftDiv">
+        <img src={book.cover} alt={book.title} className="bookCover" />
+        <button type="submit" onClick={deleteBook}>Supprimer le livre</button>
+      </div>
+      <div className="bookDescriptionDiv">
+        <h1 className="bookTitle">{book.title}</h1>
+        <p className="bookResume">Résumé :<br></br>{book.resume}</p>
+        <p>Les avis sur le livre :</p>
+        <ul className="bookReview">
+          <li>Avis 1</li>
+          <li>Avis 2</li>
+        </ul>
+        <form className="bookForm">
+          <label for="review">Laissez votre avis</label>
+          <textarea id="review" name="review"></textarea>
+          <button type="submit">Ajouter</button>
+        </form>
+      </div>
     </div>
   )
 }
